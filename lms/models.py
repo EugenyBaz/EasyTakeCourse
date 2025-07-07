fdaea2a0c21604d78aa73f06e4da09c6d169b0e1
Bazavod <eugeny.bazavod@list.ru>
2025-07-07 11:24:57 +0300

    sent invisible files

---
+++ lms/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    preview = models.ImageField(
+        upload_to="lms/preview/", blank=True, null=True, verbose_name="Превью"
+    )
+    description = models.TextField(verbose_name="Описание")
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс")
+    name = models.CharField(max_length=100, verbose_name="Название")
+    preview = models.ImageField(
+        upload_to="lms/preview/", blank=True, null=True, verbose_name="Превью"
+    )
+    description = models.TextField(verbose_name="Описание")
+    link = models.URLField(verbose_name="Ссылка на видео", blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return self.name
---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    usermame = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    avatar = models.ImageField(
+        upload_to="users/avatars/", blank=True, null=True, verbose_name="Аватар"
+    )
+    phone_number = models.CharField(
+        max_length=50, verbose_name="Телефон", help_text="Введите номер телефона"
+    )
+    city = models.CharField(max_length=50, verbose_name="Город")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
