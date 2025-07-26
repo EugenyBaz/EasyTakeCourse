f57db22c36d596c41a7adf46bb6b283684757a20
Bazavod <eugeny.bazavod@list.ru>
2025-07-26 19:43:09 +0300

    add async sending to subscribers

--- lms/tasks.py
+++ lms/tasks.py
@@ -6,30 +6,19 @@ import logging
 
 @shared_task
 def send_information_about_update_course(emails):
-    try:
-        send_mail(
-            'Test Subject',
-            'This is a test message.',
-            settings.DEFAULT_FROM_EMAIL,
-            ['eugeny.bazavod@list.ru'],  # Сюда подставьте реальный email
-            fail_silently=False
-        )
-        print("Ну, круто получил же письмо!")
-    except Exception as e:
-        print(f"Ошибка доставки письма: {e}")
     """Отправляет сообщение подписчику об обновлении курса."""
     logger = logging.getLogger(__name__)
     logger.info(f'Sending notifications to {len(emails)} subscribers.')
 
-    # Проверяем тип данных перед отправкой
-    assert isinstance(emails, list), "The argument must be a list of strings."
-
     subject = 'Обновление на курсе'
-    message = 'Вы получили это письмо так как подписались на обновления'
+    message = ('Вы получили это письмо так как подписались на обновления.\n'
+               'Зайдите в личный кабинет, что бы посмотреть изменения.')
     sender = settings.DEFAULT_FROM_EMAIL
 
     # Отправляем письма
     send_mail(subject, message, sender, emails, fail_silently=False)
+
+
 
 
 
